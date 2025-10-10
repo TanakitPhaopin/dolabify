e79e33be1dea1de8dc2cab0f2b59d40c868d7ea1
Tanakit Phaopin <ocheer12@hotmail.com>
2025-10-10 12:24:08 +0700

    Test route with params

--- backend/src/routes/test.js
+++ backend/src/routes/test.js
@@ -6,4 +6,9 @@ testRouter.get('/test', (req, res) => {
   res.send('Test route is working!');
 });
 
+testRouter.get('/test/:name', (req, res) => {
+  const { name } = req.params;
+  res.send(`Hello, ${name}! This is a test route.`);
+});
+
 export default testRouter;
