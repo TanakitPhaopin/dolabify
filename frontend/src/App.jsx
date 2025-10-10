753897ac0f0ca544db660e638143fa8b203e6561
Tanakit Phaopin <ocheer12@hotmail.com>
2025-10-10 12:30:52 +0700

    Frontend api is working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
+import API from './api'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const fetchTest = async () => {
+      try {
+        const response = await API.get('/test/ocheer');
+        console.log('Response from /test:', response.data);
+      } catch (error) {
+        console.error('Error fetching /test:', error);
+      }
+    };
+
+    fetchTest();
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Home page</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
