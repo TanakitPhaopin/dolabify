03d16cb6d764bb0608eca6d08f5537dcf257c78f
Tanakit Phaopin <ocheer12@hotmail.com>
2025-10-20 11:13:14 +0700

    Fix refresh token logic

--- frontend/src/pages/MyProjects.jsx
+++ frontend/src/pages/MyProjects.jsx
@@ -2,6 +2,9 @@ export default function MyProjects() {
     return (
         <div className="min-h-screen text-white p-8">
             <p>Welcome to My Projects!</p>
+            <p>Name</p>
+            <p>Description</p>
+            <p>Column view and list view</p>
         </div>
     );
 }
